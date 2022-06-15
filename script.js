0104e3988e849306620cb00281a263e1cbc5fbfe
unknown <mylastell>
2022-06-14 23:46:45 -0300

    concluido

---
+++ script.js
@@ -0,0 +1,34 @@
+// Crie uma variável que receba um nome;
+let nome = "Myllena"; // STRING
+console.log("O nome era:", nome);
+
+nome = "Mari";
+console.log("O nome é agora:", nome);
+
+
+// Crie uma variável que receba um número;
+const numero = 200;
+console.log("Esse número é:", numero);
+
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let anoAtual = 2022 - numero;
+console.log("O resultado é:", anoAtual);
+
+// Agora mostre no console o resultado da soma das duas variáveis 
+//guardadas anteriormente;
+const soma = anoAtual + numero;
+console.log("A soma das duas são:", soma);
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log");
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o 
+//valor booleano(true ou false) que representa verdadeiro ou falso e mostre 
+//a saída no console;
+
+let quartaFeira = true;
+console.log("O valor booleano é:", quartaFeira);
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log("O tipo de dado é:", typeof quartaFeira);
